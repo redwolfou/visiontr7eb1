65493668a27fe7db2f93605630cb504bdb907263
redwolfou <48097704+redwolfou@users.noreply.github.com>
2019-03-03 14:30:00 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,15 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const RichEmbed = require("discord.js");
+const { Client, Util } = require('discord.js');
 const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);  
-});
 
 
 client.on('guildMemberAdd', member => {
-var channel = member.guild.channels.find('551433744527917066');
+var channel = member.guild.channels.find('id', 'الايدي');
     if(!channel) return;
  setTimeout(() => {
-channel.send('** Welcome To Vision. **')
+channel.send('**WELCOME TO DREAM.**')
 }, 3000,);
 })
-
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
