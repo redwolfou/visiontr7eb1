0c6e9be36242bc746c6f69d3fecd9c8f45492f2d
redwolfou <48097704+redwolfou@users.noreply.github.com>
2019-03-03 14:56:08 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on('guildMemberAdd', member => {
+var channel = member.guild.channels.find('551433744527917066');
+    if(!channel) return;
+ setTimeout(() => {
+channel.send('**Welcome To Vision. **')
+}, 3000,);
+})
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
