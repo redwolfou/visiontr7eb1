8b43a1c9c92f9c6af82995403d98a92a56b5b189
redwolfou <48097704+redwolfou@users.noreply.github.com>
2019-03-03 19:21:21 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("551433744527917066");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**Welcome To Vision. **`), 4000)        
+}
+});
 
-client.on('guildMemberAdd', member => {
-var channel = member.guild.channels.find('551433744527917066');
-    if(!channel) return;
- setTimeout(() => {
-channel.send('**WELCOME TO Vision.**')
-}, 3000,);
-})
-
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
