22f8313143848cea8a3477e9b2ab5224021a61b6
redwolfou <48097704+redwolfou@users.noreply.github.com>
2019-03-03 14:24:08 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,16 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
 
 
 client.on('guildMemberAdd', member => {
 var channel = member.guild.channels.find('551433744527917066');
     if(!channel) return;
  setTimeout(() => {
-channel.send('**Welcome To Vision. **')
+channel.send('** Welcome To Vision. **')
 }, 3000,);
 })
 
